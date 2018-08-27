f87c17ed0e27fbed139a8af3f6f0ff6dea52a1ef
Gabriel Garcia <gabrielgsdev@gmail.com>
2018-08-27 18:38:16 -0500

    ADD my description

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,9 +4,9 @@ import Link from 'gatsby-link'
 const IndexPage = () => (
   <div>
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <p>I'm Gabriel Garcia.</p>
+    <p>I'm from Mexico</p>
+    <Link to="/page-2/">Myself</Link>
   </div>
 )
 
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -3,8 +3,8 @@ import Link from 'gatsby-link'
 
 const SecondPage = () => (
   <div>
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <h1>Frontend Developer</h1>
+    <p>I enjoy building usable and beautiful interfaces</p>
     <Link to="/">Go back to the homepage</Link>
   </div>
 )
